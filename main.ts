36e917cd588f4e537f603c968d5400d012029585
ziravfx <113920444+ziravfx@users.noreply.github.com>
2022-09-19 10:01:53 -0500

    changed radio handler

--- main.ts
+++ main.ts
@@ -1,88 +1,90 @@
-/**
- * 1 = rock
- * 
- * 2 = scissors
- * 
- * 3 = paper
- */
-radio.onReceivedNumber(function (receivedNumber) {
-    if (handsign == receivedNumber) {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-    } else if (handsign == 1 && receivedNumber == 2) {
-        showW()
-    } else if (handsign == 1 && receivedNumber == 3) {
-        showL()
-    } else if (handsign == 2 && receivedNumber == 1) {
-        showL()
-    } else if (handsign == 2 && receivedNumber == 3) {
-        showW()
-    } else if (handsign == 3 && receivedNumber == 1) {
-        showW()
-    } else {
-        showL()
-    }
-    basic.pause(5000)
-    basic.clearScreen()
-})
+let radiochannel = 0
+let radioselect = true
+radio.on()
+let selectedobject = 0
+let opponentnum = 0
+let homescore = 0
+let opponentscore = 0
+let connection = false
 function showL() {
-    basic.showLeds(`
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . . .
-        # # # . #
-        `)
+    opponentscore++
+}
+function showW() {
+    homescore++
 }
 input.onButtonPressed(Button.A, function () {
-    handsign = 1
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    radio.sendNumber(handsign)
-})
-input.onButtonPressed(Button.AB, function () {
-    handsign = 3
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    radio.sendNumber(handsign)
+    if (radioselect) {
+        radiochannel++;
+        basic.showNumber(radiochannel, 50);
+    }
+    else {
+        selectedobject = 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    handsign = 2
-    basic.showLeds(`
-        # # . . #
-        # # . # .
-        . . # . .
-        # # . # .
-        # # . . #
-        `)
-    radio.sendNumber(handsign)
+    if (radioselect) {
+        radiochannel--;
+        basic.showNumber(radiochannel, 50);
+    }
+    else {
+        selectedobject = 2
+    }
 })
-function showW() {
-    basic.showLeds(`
-        # . . . #
-        # . . . #
-        # . # . #
-        # # . # #
-        # . . . #
-        `)
-}
-let handsign = 0
-radio.setGroup(154)
-basic.forever(function () {
+input.onButtonPressed(Button.AB, function () {
+    if (radioselect) {
+        radio.setGroup(radiochannel)
+        radioselect = false
+        basic.showString("Selected: " + radiochannel, 50)
+    }
+    else {
+        selectedobject = 3
+    }
+})
+if (!(radioselect)) { while (!(connection)) { radio.sendString("waiting for connection") } }
 
+if (!(radioselect)) {
+    while (connection) {
+        basic.showNumber(3)
+        basic.pause(500)
+        basic.showNumber(2)
+        basic.pause(500)
+        basic.showNumber(1)
+        basic.pause(500)
+        while (!(selectedobject) || !(opponentnum)) {
+            basic.pause(100)
+        }
+        if (selectedobject == opponentnum) {
+            basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        } else if (selectedobject == 1 && opponentnum == 2) {
+            showW()
+        } else if (selectedobject == 1 && opponentnum == 3) {
+            showL()
+        } else if (selectedobject == 2 && opponentnum == 1) {
+            showL()
+        } else if (selectedobject == 2 && opponentnum == 3) {
+            showW()
+        } else if (selectedobject == 3 && opponentnum == 1) {
+            showW()
+        } else {
+            showL()
+        }
+        basic.showString(homescore + " - " + opponentscore)
+        selectedobject = 0
+        opponentnum = 0
+    }
+}
+
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "waiting for connection") {
+        connection = true
+    }
+})
+radio.onReceivedNumber(function (receivedNumber) {
+    opponentnum = receivedNumber
 })
